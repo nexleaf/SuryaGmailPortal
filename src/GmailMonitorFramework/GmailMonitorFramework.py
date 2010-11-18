772c62d1a826666640282883889b28d708b27380
Nishanth Alapati <nish@cs.ucla.edu>
2010-11-18 11:45:35 -0800

    Generic gmail poller and result mailer

---
+++ src/GmailMonitorFramework/GmailMonitorFramework.py
@@ -0,0 +1,47 @@
+'''
+Created on Nov 15, 2010
+
+@author: surya
+'''
+import time
+import logging
+
+from Logging.Logger import getLog
+from Locking.AppLock import getLock
+
+class GmailMonitorFramework:
+    ''' This class forms the base class for any applications that intend to poll gmail.
+    '''
+    
+    log = getLog("GmailMonitor")
+    gmontags = ' GMAILMONITOR '
+                
+    def checkInbox(self):
+        ''' Implementors must override this method in order to implement their custom
+            polling application 
+        '''
+    
+    def run(self, pidfile, programname, timeinterval):
+        """ This method runs the danaJob every timeinterval subject to the 
+            constraint that if force is True, we process all the images
+            that were already processed.
+            
+            Keyword Arguments:
+            pidfile      -- Ensure that only one Application specific dana runs
+                            at a time.
+            programname  -- Name of the application specific dana.
+            timeinterval -- an integer time in seconds after which to repeat dana
+        """
+        
+        if not getLock(pidfile, programname):
+            return
+        
+        while True:
+            try:
+                self.log.info("Running Gmail Monitor", extra=self.gmontags)
+                self.checkInbox()
+                self.log.info("Done Running Gmail Monitor", extra=self.gmontags)
+            except Exception, err:
+                self.log.critical("Error Checking Inbox " + str(err), extra=self.gmontags)
+            time.sleep(timeinterval)
+            
---
+++ src/GmailResultsFramework/GmailResultsFramework.py
@@ -0,0 +1,55 @@
+'''
+Created on Nov 15, 2010
+
+@author: surya
+'''
+import time
+import logging
+
+from Logging.Logger import getLog
+from Locking.AppLock import getLock
+
+class GmailResultsFramework:
+    ''' This class forms the base class for any applications that need to mail results from the database.
+    '''
+    
+    log = getLog("GmailResults")
+    
+    def __init__(self, level=logging.DEBUG):
+        ''' Constructor
+        
+            Keyword Arguments:
+            level -- The logging level.
+        '''
+        
+        self.grestags = ' GMAILRESULTS '
+        self.log.setLevel(level)
+        
+    def checkResults(self):
+        ''' Implementors must override this method in order to implement their custom
+            result mailing application 
+        '''
+        
+    def run(self, pidfile, programname, timeinterval):
+        """ This method runs the danaJob every timeinterval subject to the 
+            constraint that if force is True, we process all the images
+            that were already processed.
+            
+            Keyword Arguments:
+            pidfile      -- Ensure that only one Application specific dana runs
+                            at a time.
+            programname  -- Name of the application specific dana.
+            timeinterval -- an integer time in seconds after which to repeat dana
+        """
+        
+        if not getLock(pidfile, programname):
+            return
+        
+        while True:
+            try:
+                self.log.info("Running Gmail Result Mailer", extra=self.grestags)
+                self.checkResults()
+                self.log.info("Done Running Gmail Result Mailer", extra=self.grestags)
+            except Exception, err:
+                self.log.critical("Error Checking Results " + str(err), extra=self.grestags)
+            time.sleep(timeinterval)
